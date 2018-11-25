427fd9065de5509bf46800a5159f52b77027abac
Leon Frempong (fremponl) <fremponl@coventry.ac.uk>
2018-11-25 19:37:32 +0000

    Q3 v4

--- Q3 Coursework.py
+++ Q3 Coursework.py
@@ -1,5 +1,5 @@
 # Undirected, unweighed graph with Adjency Lists
-class vertex:
+class Vertex:
     def __init__(self, n):
         self.name = n
         self.adjacent = list() #adjacent are connected vertices
@@ -11,26 +11,42 @@ class vertex:
         
         
 class graph:
-    nodes = {}
-    #adding  a node (vertex)
+    vertices = {} #vertices is a dtionary, so 
+    #adding  a node (vertex) to the graph
     #checks if vertex passed in is actually a vertex object and doesn't exist in the dictionary yet.
-    def add_node(self, node):
-        if  (node,Node) and (node.name) not in self.vertices:
-            self.vertices[node.name] = node
+    def add_node(self, vertex):
+        if  (vertex,Vertex) and (vertex.name) not in self.vertices:
+            self.vertices[vertex.name] = vertex
             return True
         else:
             return False
+    #Adding an edge to the graph
     #adding an edge means updating the adjency list of both nodes with the other one
-    def add_edge(self, )
+    def add_edge(self, start, end):
+        if start in self.vertices and end in self.vertices:
+            self.vertices[start].add_adjacent(end)
+            self.vertices[end].add_adjacent(start)  
+            return True
+        else:
+            return False
 
-    #a. Adding a node to the graph.
-    #b. Adding an edge to the graph.
+
     #c. Printing the graph.
-
+    def print_graph(self):
+        for key in sorted(list(self.vertices.keys())):
+            print(key + str(self.vertices[key].adjacent))
 
 #visualisation 
-g = graph(v,e)
-v = vertex('1')
+#g = graph(v,e)
+g = graph()
+a = Vertex('1')
+g.add_node(a)
+g.add_node(Vertex('2'))
 
+for i in range(ord('1'), ord('5')): #it iterates the nodes in the graph
+    g.add_node(Vertex(chr(i)))
 
-edges = [ ['12'], ['24'], ['34'], ['35'], ['45'] ]
+edges = ['12', '24', '34', '35', '45']
+for edge in edges:
+    g.add_edge(edge[:1], edge[1:])
+g.print_graph()
