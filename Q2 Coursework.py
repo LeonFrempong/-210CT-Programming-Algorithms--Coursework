323440d30f6534ff044726614840579bf692619b
Leon Frempong (fremponl) <fremponl@coventry.ac.uk>
2018-11-30 04:32:08 +0000

    v4

--- Q2 Coursework.py
+++ Q2 Coursework.py
@@ -22,8 +22,6 @@ def tree_insert( tree, item):
                 tree_insert(tree.right,item)
     return tree
 
-
-
 def postorder(tree):
     if(tree.left!=None):
         postorder(tree.left)
@@ -44,44 +42,23 @@ pre_order function outputs the items by following the left tree, then right tree
 
 def pre_order(tree):
     print(tree.value)
-    if tree.left != None:
+    if (tree.left != None):
         pre_order(tree.left)
-    if tree.right != None:
+    if (tree.right != None):
         pre_order(tree.right)
+
+
+def FileOpen():
+    openf = open("EnglishNodes.txt" ,"r")
+    openf = openf.read().lower().split()
+    return openf
+
+Openfile = FileOpen()
+
+t = tree_insert(None,Openfile[0])
+for i in range(len(Openfile)):
+      tree_insert(t,Openfile[i])
+pre_order(t)
+
     
-if __name__ == '__main__':
-    englishNodes_file = open("EnglishNodes.txt", "r")  # file used for th binary search
-        # print(englishNodes_file.read())
-    message = englishNodes_file.read()
-    print(message)
-    englishNodes_file.close()
 
-    #for englishNodes_file in file:
-     #   fields = englishNodes_file.split(",")
-            
-      #  fields1 = fields[0]
-       # fields2 = fields[1]
-        #fields2 = fields[2]
-        #fields2 = fields[2]
-        #print (fields1 + " " + fields2 + " " + fields3)
-# https://teamtreehouse.com/community/i-get-this-error-message-bummer-typeerror-type-object-is-not-iterable
-
-#implement lists through split
-#iterative 
-    words = message.split(" ")
-    print(words)
-     for i in range()
-  #t=tree_insert(None,'My');
-  #tree_insert(t,'name')
-  #tree_insert(t,'is')
-  #tree_insert(t,'Leon')
-  #tree_insert(t,'Frempong')
-  #tree_insert(t,'and')
-  #tree_insert(t,'i')
-  #tree_insert(t,'am')
-  #tree_insert(t,'nineteen')
-  #tree_insert(t,'years')
-  #tree_insert(t,'old')
-    in_order(tree)
-
-    pre_order(tree)
