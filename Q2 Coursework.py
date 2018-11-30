c72543f6db84f56da611011cd014de43a5a9a464
Leon Frempong (fremponl) <fremponl@coventry.ac.uk>
2018-11-30 08:19:59 +0000

    v5

--- Q2 Coursework.py
+++ Q2 Coursework.py
@@ -1,3 +1,9 @@
+"""
+Title:Binary Search Tree
+Author: Hintea, D
+Date: 2018
+Availability: http://moodle.coventry.ac.uk 
+"""
 class BinTreeNode(object):
  
     def __init__(self, value):
@@ -40,25 +46,26 @@ def in_order(tree):
 pre_order function outputs the items by following the left tree, then right tree 
 """
 
-def pre_order(tree):
-    print(tree.value)
+def pre_order(tree): # this function first outputs the item, follows the left then the right tree
+    print(tree.value) # prints the head node
     if (tree.left != None):
-        pre_order(tree.left)
+        pre_order(tree.left)#constantly goes left side when there are values 
     if (tree.right != None):
-        pre_order(tree.right)
+        pre_order(tree.right)#constantly goes right where are values 
 
 
-def FileOpen():
-    openf = open("EnglishNodes.txt" ,"r")
-    openf = openf.read().lower().split()
+def FileOpen(): # this function opens and reads the file
+    openf = open("EnglishNodes.txt" ,"r") 
+    openf = openf.read().upper().split() #opens the file with capital letter and splits the words
     return openf
 
 Openfile = FileOpen()
 
 t = tree_insert(None,Openfile[0])
-for i in range(len(Openfile)):
-      tree_insert(t,Openfile[i])
-pre_order(t)
+for i in range(len(Openfile)):#finds the lengths of the file
+      tree_insert(t,Openfile[i])#calls the function 
+pre_order(t) # prints in pre_order 
+
 
     
 
